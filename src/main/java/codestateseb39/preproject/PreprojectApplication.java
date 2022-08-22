d80df4d276c4b9d0be476f36aa72a5f4eebc0c8a
kwangsooPark92 <109050525+kwangsooPark92@users.noreply.github.com>
2022-08-22 16:16:21 +0900

    initial commit

---
+++ src/main/java/codestateseb39/preproject/PreprojectApplication.java
@@ -0,0 +1,13 @@
+package codestateseb39.preproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreprojectApplication.class, args);
+	}
+
+}
